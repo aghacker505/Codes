3508bb91856eae2f5f4f001989404a3c2c5f1948
aghacker <insuabhay951@gmail.com>
2022-09-21 11:55:38 +0530

    added

---
+++ java/inheritance/Num.java
@@ -0,0 +1,28 @@
+class Num   // parent class
+{
+    int a, b ;
+    void get_num()
+    {
+        a = 5;
+        b = 6;
+    }
+}
+
+class Add extends Num
+{
+    int c;
+    void get_num()
+    {
+        get_num();
+        c = a+b;
+        System.out.println(c);
+    }
+}
+class Ex_single_inherit
+{
+    public static void main(String[] args) {
+        
+        Add A1 = new Add();
+        A1.get_num();
+    }
+}
